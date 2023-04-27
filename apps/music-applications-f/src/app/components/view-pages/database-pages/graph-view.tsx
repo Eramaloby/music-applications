3e329e2f4ead6e7dc84fb4062680ffa1e33543f0
Kirill <kirill@.com>
2023-04-27 20:09:44 +0300

    fix graph page

--- apps/music-applications-f/src/app/components/view-pages/database-pages/graph-view.tsx
+++ apps/music-applications-f/src/app/components/view-pages/database-pages/graph-view.tsx
@@ -1,72 +1,85 @@
 import {
+  AlbumProperties,
   Neo4jDbItem,
+  PlaylistProperties,
+  TrackProperties,
 } from '../../../types';
-import { GraphCanvas, darkTheme } from 'reagraph';
+import { useRef } from 'react';
+import { GraphCanvas, darkTheme, GraphCanvasRef, useSelection } from 'reagraph';
 
 const GraphViewPage = ({ item }: { item: Neo4jDbItem }) => {
-  const nodes = [];
-  const edges = [];
+  const nodes_g = [];
+  const edges_g = [];
   const RED = '#ff3333';
   const BLUE = '#5c5cff';
   const GREEN = '#04bd3b';
   const YELLOW = '#d9d507';
   const GRAY = '#f5770a';
 
-  nodes.push({ id: '-1', label: item.properties.name });
+  nodes_g.push({ id: '-1', label: item.properties.name });
+  //добавить проверку на входимость в сущность
+
   
-  edges.push({
+  edges_g.push({
     source: '-1',
     target: '-2',
     id: '-1 -> -2',
     label: 'Edge -1 -> -2',
     size: 2,
   });
-  edges.push({
+  edges_g.push({
     source: '-1',
     target: '-3',
     id: '-1 -> -3',
     label: 'Edge -1 -> -3',
     size: 2,
   });
-  edges.push({
+  edges_g.push({
     source: '-1',
     target: '-4',
     id: '-1 -> -4',
     label: 'Edge -1 -> -4',
     size: 2,
   });
-  edges.push({
+  edges_g.push({
     source: '-1',
     target: '-5',
     id: '-1 -> -5',
     label: 'Edge -1 -> -5',
     size: 2,
   });
-  edges.push({
+  edges_g.push({
     source: '-1',
     target: '-6',
     id: '-1 -> -6',
     label: 'Edge -1 -> -6',
     size: 2,
   });
+  const graphRef = useRef<GraphCanvasRef | null>(null);
+  const { actives, selections, onNodeClick, onCanvasClick, onLasso, onLassoEnd } = useSelection({
+    ref: graphRef,
+    nodes: nodes_g,
+    edges: edges_g,
+    type: 'multi'
+  });
   return (
     <div>
       <div>
         {item.relations.map((relation: any, index: number) => {
           if (relation.target.type === 'Track') {
-            nodes.push({ id: '-2', label: "Track", fill: RED, });
+            nodes_g.push({ id: '-2', label: "Track", fill: RED, });
           }
           else if (relation.target.type === 'Artist') {
-            nodes.push({ id: '-3', label: "Artist", fill: BLUE, });
+            nodes_g.push({ id: '-3', label: "Artist", fill: BLUE, });
           }
           else if (relation.target.type === 'Album') {
-            nodes.push({ id: '-4', label: "Album", fill: GREEN, });
+            nodes_g.push({ id: '-4', label: "Album", fill: GREEN, });
           }
           else if (relation.target.type === 'Genre') {
-            nodes.push({ id: '-5', label: "Genre", fill: YELLOW, });
+            nodes_g.push({ id: '-5', label: "Genre", fill: YELLOW, });
           }
           else if (relation.target.type === 'Playlist') {
-            nodes.push({ id: '-6', label: "Playlist", fill: GRAY, });
+            nodes_g.push({ id: '-6', label: "Playlist", fill: GRAY, });
           }
           return <></>;
         })}
@@ -95,12 +108,12 @@ const GraphViewPage = ({ item }: { item: Neo4jDbItem }) => {
             type = '-6';
           }
 
-          nodes.push({
+          nodes_g.push({
             id: String(index),
             label: relation.target.properties.name,
             fill: fill,
           });
-          edges.push({
+          edges_g.push({
             source: type,
             target: String(index),
             id: '-1 -> ' + index,
@@ -112,10 +125,19 @@ const GraphViewPage = ({ item }: { item: Neo4jDbItem }) => {
       </div>
       <div className="database-graph-form">
         <GraphCanvas
+          ref={graphRef}
+          draggable
+          selections={selections}
+          actives={actives}
+          onNodeClick={onNodeClick}
+          onCanvasClick={onCanvasClick}
+          lassoType="node"
+          onLasso={onLasso}
+          onLassoEnd={onLassoEnd}
           // layoutType="radialOut2d"
           labelFontUrl='https://ey2pz3.csb.app/NotoSansSC-Regular.ttf'
-          nodes={nodes}
-          edges={edges}
+          nodes={nodes_g}
+          edges={edges_g}
           onEdgeClick={(edge) => alert(`${edge.label}`)}
           contextMenu={({ data, onClose }) => (
             <div
@@ -133,7 +155,7 @@ const GraphViewPage = ({ item }: { item: Neo4jDbItem }) => {
               <button onClick={onClose}>Close Menu</button>
             </div>
           )}
-          labelType="nodes"
+          labelType="all"
           theme={darkTheme}
         />
       </div>
