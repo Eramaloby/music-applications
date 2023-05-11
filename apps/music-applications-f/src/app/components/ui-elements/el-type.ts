9f3e25cedddcd67d23a3f169c369b38156ca32da
Zhekicho1d <zheka5438@gmail.com>
2023-05-11 23:49:26 +0300

    fix errors

---
+++ apps/music-applications-f/src/app/components/ui-elements/el-type.ts
@@ -0,0 +1,5 @@
+export interface Element {
+    src: string;
+    header: string;
+    content: string;
+};
--- apps/music-applications-f/src/app/pages/home/home.tsx
+++ apps/music-applications-f/src/app/pages/home/home.tsx
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+import background_image from '../../../assets/backgr.png'
+
 import './home.styles.scss';
 import { useNavigate } from 'react-router-dom';
+
+import ElementList from '../../components/ui-elements/el-list';
+
 
 const HomePage = () => {
   const router = useNavigate();
@@ -11,6 +16,20 @@ const HomePage = () => {
     nodesCount: 0,
     relCount: 0,
   });
+
+  const [info, setInfo] = useState([
+    {header: 'User system', src:'../../../assets/user.png', content: `System provides a high level of security, protecting users' personal data
+    and content from unauthorized access. Make your experience more secure`},
+    {header: 'Recommendations', src:'../../../assets/like.png', content: `Tired of music in your playlist?
+    Based on your recent preferences, you will receive many recommendations,
+    that will always help you discover something new`},
+    {header: 'Knowledge base',src:'../../../assets/connection.png', content: `Constantly expanding and improving the graph knowledge base will allow you to
+    quickly and efficiently search for the queries you need, as well as navigate
+    and use related elements.`},
+    {header: 'Explore Web', src:'../../../assets/spotify.png', content: `In case you still lack the knowledge base, you can use the database,
+    provided by the Spotify API. Listen to tracks, get lyrics, add entities
+    to the graph knowledge base and much more`},
+  ])
 
   const fetchResults = async () => {
     axios.get('http://localhost:4200/api/db-stats').then((response) => {
@@ -27,27 +46,20 @@ const HomePage = () => {
   // };
 
   return (
-    <div className="home-page-wrapper">
+    <div className="home-page-wrapper" style={{'backgroundImage': `url(${background_image})`}}>
+      <div className='home-page-welcome'>
+        <h1>Welcome to ostis-music</h1>
+        <h3>Join the multi-million user family</h3>
+      </div>
       <div className="home-page-db-stats">
-        {stats.nodesCount ? (
-          <div className="db-stats-count-of-nodes">
-            Count of nodes in db:{' '}
-            <span className="stats-container">{stats.nodesCount}</span>
-          </div>
-        ) : (
-          <div className="db-stats-count-of-nodes">Wait...</div>
-        )}
-        {stats.relCount ? (
-          <div className="db-stats-count-of-relationships">
-            Count of relationships in db:{' '}
-            <span className="stats-container">{stats.relCount}</span>
-          </div>
-        ) : (
-          <div className="db-stats-count-of-relationships"></div>
-        )}
+          <ElementList items={info}/>
         <div className="refresh-button-container"></div>
       </div>
       <div className='info-container'>
+        <div className='text'>
+          This and even more you can recieve after autorisation.
+          Sign in and get a extended personalized experience in our service.
+        </div>
         <div className="link" onClick={() => router('/signin')}>
           Sign in
         </div>
