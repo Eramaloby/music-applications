c83f286a1b54013715e498ce92171f238180d7e9
Yauheni Shaikouski <eugeneshaikovskiy@gmail.com>
2023-10-16 20:45:12 +0300

    add partial artist form

--- apps/music-applications-f/src/app/components/profile/artist-form/artist-form.component.tsx
+++ apps/music-applications-f/src/app/components/profile/artist-form/artist-form.component.tsx
@@ -1,9 +1,138 @@
-import React from 'react'
+import { TextField, Tooltip } from '@mui/material';
+import React, { useState } from 'react';
+import { validateFieldRequiredNotEmpty } from '../../../utils';
 
-const ArtistForm = () => {
-  return (
-    <div>ArtistForm</div>
-  )
+export interface ArtistFormFields {
+  artistName: string;
+  artistType: string;
+  artistDescription: string;
+  artistImageBase64: string;
+
+  artistRelatedGenreIds: number[];
 }
 
-export default ArtistForm
+export interface ArtistFormFieldsErrors {
+  artistName: string;
+  artistType: string;
+  artistDescription: string;
+  artistImageBase64: string;
+  artistRelatedGenreIds: string;
+}
+
+const ArtistForm = () => {
+  const [form, setForm] = useState<ArtistFormFields>({
+    artistName: '',
+    artistType: '',
+    artistDescription: '',
+    artistImageBase64: '',
+    artistRelatedGenreIds: [],
+  });
+
+  const [errors, setErrors] = useState<ArtistFormFieldsErrors>({
+    artistName: '',
+    artistType: '',
+    artistDescription: '',
+    artistImageBase64: '',
+    artistRelatedGenreIds: '',
+  });
+
+  const onFormControlsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+
+    setErrors({
+      ...errors,
+      [event.target.name]: validateFieldRequiredNotEmpty(
+        event.target.value,
+        event.target.name
+      ),
+    });
+  };
+
+  return (
+    <div className="form-wrapper">
+      <div className="form-header">Fill required fields</div>
+      <div className="form-controls">
+        <div className="control-wrapper">
+          <Tooltip
+            title="Artist name, real person name, band or group name"
+            placement="top"
+          >
+            <TextField
+              name="artistName"
+              className="form-value-input"
+              InputLabelProps={{ style: { color: 'white', fontWeight: '500' } }}
+              inputProps={{
+                style: {
+                  color: 'white',
+                  fontWeight: '400',
+                  height: '15px',
+                },
+              }}
+              color={'primary'}
+              label={'Enter artist name'}
+              value={form.artistName}
+              onChange={onFormControlsChange}
+              error={Boolean(errors.artistName)}
+              helperText={errors.artistName}
+            ></TextField>
+          </Tooltip>
+        </div>
+        <div className="control-wrapper">
+          <Tooltip
+            title="Could be singer, band, guitarist, etc"
+            placement="top"
+          >
+            <TextField
+              name="artistType"
+              className="form-value-input"
+              InputLabelProps={{ style: { color: 'white', fontWeight: '500' } }}
+              inputProps={{
+                style: {
+                  color: 'white',
+                  fontWeight: '400',
+                  height: '15px',
+                },
+              }}
+              color={'primary'}
+              label={'Enter artist type'}
+              value={form.artistType}
+              onChange={onFormControlsChange}
+              error={Boolean(errors.artistType)}
+              helperText={errors.artistType}
+            ></TextField>
+          </Tooltip>
+        </div>
+        <div className="control-wrapper">
+          <Tooltip
+            title="Something that would describe an artist or short biography"
+            placement="top"
+          >
+            <TextField
+              name="artistDescription"
+              className="form-value-input"
+              InputLabelProps={{ style: { color: 'white', fontWeight: '500' } }}
+              inputProps={{
+                style: {
+                  color: 'white',
+                  fontWeight: '400',
+                  height: '15px',
+                },
+              }}
+              color={'primary'}
+              label={'Enter artist description'}
+              value={form.artistDescription}
+              onChange={onFormControlsChange}
+              error={Boolean(errors.artistDescription)}
+              helperText={errors.artistDescription}
+            ></TextField>
+          </Tooltip>
+        </div>
+        <div className="control-wrapper">
+          {/* Selector for existing genres inside db */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArtistForm;
