666553c4605496a60682e08dc43ed5ce306956a5
Yauheni Shaikouski <eugeneshaikovskiy@gmail.com>
2023-11-13 15:04:36 +0300

    fix null user columns

--- apps/music-applications-b/src/auth/user.repository.ts
+++ apps/music-applications-b/src/auth/user.repository.ts
@@ -41,8 +41,8 @@ export const customUserRepository: Pick<UserRepository, any> = {
       tasks: [],
       relationshipsAddedCount: 0,
       nodesAddedCount: 0,
-      subscribers: '',
-      subscriptions: '',
+      subscribers: '[]',
+      subscriptions: '[]',
     });
 
     try {
